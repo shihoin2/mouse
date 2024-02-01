cdddca9025252810ab62921f3df52104f0776fa6
SayuriOkuyama <mosyaneko77@gmail.com>
2024-02-01 10:10:07 +0900

    使わなかったファイルを削除

--- routes/api.php
+++ routes/api.php
@@ -24,19 +24,3 @@ Route::prefix('vision_boards')
     Route::put('/{id}', 'update')->name('update');
     Route::delete('/{id}', 'destroy')->name('destroy');
   });
-
-Route::prefix('texts')
-  ->name('texts.')
-  ->controller(TextController::class)
-  ->group(function () {
-    // Route::get('/', 'index')->name('index');
-    // ボード新規作成時に６個同時に作成してボードと紐付ける
-    Route::post('/', 'create')->name('create');
-    // Route::post('/', 'store')->name('store');
-    // ボードのedit ルートと同時に、該当ボードのテキスト取得
-    Route::get('/{id}', 'edit')->name('edit');
-    // 随時更新
-    Route::put('/{id}', 'update')->name('update');
-    // ボード削除時に同時削除
-    Route::delete('/{id}', 'destroy')->name('destroy');
-  });
